f4e90e6d2ab74a38dd2183674a80805ec9fdb4ad
Dominic Lagle <dominic.lagle444@gmail.com>
2024-12-11 12:40:01 -0800

    Initial Commit

---
+++ hangman_main.py
@@ -0,0 +1,10 @@
+# Main file for the hangman game 
+# Owner is Dominic Lagle
+# This is the project follow through for Python Class 1.0
+# Version 0.0
+
+
+class Hangman():
+    def __init__():
+        life = 0
+
